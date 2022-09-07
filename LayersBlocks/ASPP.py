fa476d24cc57c7fd44c548a1d31b297bc6ae088e
Victor Rocco <victor_rocco@hotmail.com>
2022-09-07 16:00:48 -0300

    V0.2.8: ASPP with residual option.

--- LayersBlocks/ASPP.py
+++ LayersBlocks/ASPP.py
@@ -24,6 +24,7 @@ class ASPP(tf.keras.layers.Layer):
                  padding="symmetric", # same, valid, symmetric, reflect
                  activation="LR010",  # LR010=LeakyReLU(0.10), RELU=ReLU, None
                  normalization="IN",  # IN=InstanceNormalization, BN=BatchNormalization, None
+                 residual=False, #False, True=residual add
                  l2_value=0.001, **kwargs):
 
         super().__init__(**kwargs)
@@ -34,6 +35,7 @@ class ASPP(tf.keras.layers.Layer):
         self.padding = padding
         self.activation = activation
         self.normalization = normalization
+        self.residual = residual
         self.l2_value = l2_value
 
         self.f_stdcna_by_rate = {}
@@ -43,6 +45,13 @@ class ASPP(tf.keras.layers.Layer):
                                                  padding=self.padding, activation=self.activation, l2_value=self.l2_value)
 
         self.f_add = Add()
+    def build(self, input_shape):
+
+        # Si es necesario ajusto la cantidad de filtros finales
+        # para poder hacer el Residual ADD (sin activacion)
+        self.input_channels = input_shape[-1]
+        if self.num_out_filters != self.input_channels:
+            self.f_output_conv2d_num_filters = Conv2D(filters=self.num_out_filters, kernel_size=(1, 1))
 
     def call(self, X):
 
@@ -53,6 +62,14 @@ class ASPP(tf.keras.layers.Layer):
             aspp_operations_by_rate.append(aspp_operation)
             # print("in", X.shape, "out", aspp_operation.shape, "kernel", self.kernel_size, "strides", self.strides,
             #      "rate", rate, flush=True)
+
+        if self.residual == True:
+            # Si es necesario ajusto la cantidad de filtros finales para poder hacer el Residual ADD
+            if self.num_out_filters != self.input_channels:
+                aspp_operations_by_rate.append(self.f_output_conv2d_num_filters(X))
+            else:
+                aspp_operations_by_rate.append(X)
+
         Y = self.f_add(aspp_operations_by_rate)
         return Y
 
@@ -66,5 +83,6 @@ class ASPP(tf.keras.layers.Layer):
         config["padding"] = self.padding
         config["activation"] = self.activation
         config["normalization"] = self.normalization
+        config["residual"] = self.residual
         config["l2_value"] = self.l2_value
         return config
